2977e64693712df78d25507d5a43ab434cdaff90
reziqvins <nreziq@gmail.com>
2021-10-28 20:01:07 +0700

    first commit

---
+++ src/main/kotlin/pertemuan1.kt
@@ -0,0 +1,48 @@
+ fun main (){
+     var kardus = "Buku"
+     kardus = "Pulpen"
+     println(kardus)
+
+//     val Ember ="air"
+//     Ember = "ak"
+
+     // Bilangan Bulat
+     var byte :Byte =120
+     var short : Short = 32_000
+     var int : Int = 100_000_000
+     var long  = 500_000_000L
+     println(byte)
+     println(short)
+     println(int)
+     println(long)
+
+     //Bilangan Desimal
+     var float : Float =3.4F
+     var double = 3.111232334
+     println(float)
+     println(double)
+
+     //tipe data char
+     var char : Char ='A'
+     println(char)
+
+         //tipe data stirng
+     var nama : String = "aku"
+
+     var alamat : String = """disana disini aku """
+     var alamat2 : String = """
+         aku
+         saya
+         anda
+         kamu
+     """.trimIndent()
+     println (nama)
+     println(alamat)
+     println(alamat2)
+
+     // tipe data boolean
+     var benar : Boolean = true
+     var salah : Boolean = false
+     println(benar)
+     println(salah)
+ }
